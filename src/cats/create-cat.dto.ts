0dc26f435a7c1a1fc31239f67e20ff5a69352962
Saroj <jmestmnag@gmail.com>
2023-12-22 07:51:55 +0545

    BASIC - PIPE concpet `zod`,`class-validator`

--- src/cats/create-cat.dto.ts
+++ src/cats/create-cat.dto.ts
@@ -1,5 +1,28 @@
+import { z } from 'zod';
+import { IsString, IsInt, Length, Max, Min, IsEmail } from 'class-validator';
+// creating validation through zod
+export const createCatSchema = z
+  .object({
+    name: z.string(),
+    age: z.number(),
+    breed: z.string(),
+  })
+  .required();
+export type CreateCatDtoT = z.infer<typeof createCatSchema>;
+
+// creating validation through class validator
 export class CreateCatDto {
+  @Length(10, 20)
   name: string;
+
+  @IsInt()
+  @Min(0)
+  @Max(10)
   age: number;
+
+  @IsString()
   breed: string;
+
+  @IsEmail()
+  email: string;
 }
