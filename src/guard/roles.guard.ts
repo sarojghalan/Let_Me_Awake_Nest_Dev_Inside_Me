f76bef4c647a70408ba7d16013b03360eb6d76c7
Saroj <jmestmnag@gmail.com>
2023-12-23 23:25:25 +0545

    roles guard updated with reflector and roles decorator

--- src/guard/roles.guard.ts
+++ src/guard/roles.guard.ts
@@ -1,9 +1,19 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Roles } from './roles.decorator';
 
 @Injectable()
 // for now it allows all request to proceed
 export class RolesGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
-    return true;
+  constructor(private reflector: Reflector) {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get(Roles, context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return user;
   }
 }
